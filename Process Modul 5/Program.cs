882f2200f3b23aacc7057041b6baf6f329c22dcd
Albina-K <kamalieva.albish@mail.ru>
2024-03-17 20:08:48 +0300

    рекурсия

--- Process Modul 5/Program.cs
+++ Process Modul 5/Program.cs
@@ -26,11 +26,45 @@ class MainClass
         {
             modif = modif.Remove(0, 2);
         }
+        Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." +  modif);
         if (deep > 1) 
         {
         Echo(modif, deep - 1);
         }
+    }
+    static int Factorial(int x)
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+            
+            
+    }
+    private static int hhh(int N, byte pow)
+    {
+        Console.WriteLine("Введите число");
+        var noom = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите степень");
+        var naam = Convert.ToInt32(Console.ReadLine());
+       
+        if (naam == 0)
+        {
+            return 0;
+        }
+        if (naam == 1)
+        {
+
+            return noom;
+
+        }
+        return naam;
+
     }
     
 }
